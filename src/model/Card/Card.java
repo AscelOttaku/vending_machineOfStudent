b98cac68f60bc2794e5c46fd4a6e0103d3c01f95
Kanybek <zhanybek20065732@gmail.com>
2024-12-13 02:34:09 +0600

    Added setter and Getter

--- src/model/Card/Card.java
+++ src/model/Card/Card.java
@@ -3,9 +3,22 @@ package model.Card;
 public class Card {
     private String cardName;
     private String password;
+    private double balance;
 
-    public Card(String cardName, String password) {
+    public Card(String cardName, String password, double balance) {
         this.cardName = cardName;
         this.password = password;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public double getBalance() {
+        return balance;
     }
 }
