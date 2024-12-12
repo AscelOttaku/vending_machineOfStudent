d0796fa0fd5ba91a6807841d9a6e88f918aa2ff1
Kanybek <zhanybek20065732@gmail.com>
2024-12-13 02:49:14 +0600

    getUserInput method modified

--- src/Service/MoneyReceiveWithCash.java
+++ src/Service/MoneyReceiveWithCash.java
@@ -11,19 +11,26 @@ public class MoneyReceiveWithCash extends MoneyReceiverSystem {
     public void getUserInput() {
         Scanner sc = new Scanner(System.in);
 
-        System.out.println("Enter your cash");
+        while (true) {
+            System.out.println("Enter your cash");
 
-        try {
-            int cash = sc.nextInt();
+            try {
+                int cash = sc.nextInt();
 
-            if (cash < 0 || cash > 4000) {
-                throw new IllegalArgumentException("Cash should be between 0 and 4000");
+                if (cash < 0 || cash > 4000) {
+                    throw new IllegalArgumentException("Cash should be between 0 and 4000");
+                }
+
+                this.cash = cash;
+                return;
+
+            } catch (IllegalArgumentException e) {
+                sc.nextLine();
+                String msg = e.getMessage() == null ? "undefined": e.getMessage();
+                System.out.println("Invalid input: ");
+                System.out.println("Enter Integer Number");
+                System.out.println("Cause: " + msg);
             }
-        } catch (IllegalArgumentException e) {
-            String msg = e.getMessage() == null ? "undefined": e.getMessage();
-            System.out.println("Invalid input: ");
-            System.out.println("Enter Integer Number");
-            System.out.println("Cause: " + msg);
         }
     }
 
