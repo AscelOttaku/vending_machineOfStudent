0c267bd85da26da06fcdb87d4ac196ec12f1799c
Kanybek <zhanybek20065732@gmail.com>
2024-12-13 02:45:24 +0600

    new abstract method transfer

--- src/Service/MoneyReceiverSystem.java
+++ src/Service/MoneyReceiverSystem.java
@@ -1,6 +1,10 @@
 package Service;
 
+import model.Products.CoinAcceptor;
+
 public abstract class MoneyReceiverSystem {
+
+    public abstract void transfer(CoinAcceptor coinAcceptor);
 
     public abstract void getUserInput();
 }
